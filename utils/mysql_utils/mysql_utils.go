a80a5651e89707e9ae68995c4758f69931bc8bed
Serhat Damlica <serhat.damlica@skybettingandgaming.com>
2020-12-11 17:36:56 +0000

    Add mysql error utility

---
+++ utils/mysql_utils/mysql_utils.go
@@ -0,0 +1,29 @@
+package mysql_utils
+
+import (
+	"strings"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/msd79/bookstore_users-api/utils/errors"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	errorNoRows      = "no rows in result set"
+)
+
+//ParseError parses mysql errors and return an appropirate response
+func ParseError(err error) *errors.RestErr {
+	sqlErr, ok := err.(*mysql.MySQLError)
+	if !ok {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError("No matching record found for the ID")
+		}
+	}
+
+	switch sqlErr.Number {
+	case 1062:
+		return errors.NewInternalServerError("invalid data")
+	}
+	return errors.NewInternalServerError("Error proccessing the request")
+}
